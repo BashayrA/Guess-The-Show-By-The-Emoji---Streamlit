0ae25e4009193ccf0310f794c3eeeb725499eacf
Bashayr <alharbi.bashayr@outlook.com>
2025-07-07 02:19:20 +0300

    [FIX] now answers are checked if user write one true word of the answer and the show names comparsion are logically correct & added a scoring points

--- source_code.py
+++ source_code.py
@@ -3,15 +3,15 @@ import random
 import time
 import os
 
-def launch_game(path: str):
+def launch_game(path: str, progress = 0):
     '''Control the flow of the game and choosing a random Show for the player to guess'''
-    
+
     # choosing a show randomly from the file and storing its name and emojies
     games_list = open(path, encoding="utf-8").readlines()
     index = random.randint(0, len(games_list)-1)
     show, emoji = games_list[index].split('/')
     emoji.strip()
-    show.strip()
+    show.strip().lower()
     
     # display the emojies, the text input box, and the button 
     st.write(emoji)
@@ -21,19 +21,26 @@ def launch_game(path: str):
     # check if the session_state is empty then add a 'name' attribute to store show names and add the first appearing show
     if 'show' not in st.session_state:
         st.session_state['show'] = []
-        st.session_state['show'].append(show.lower())
-
-    if value in st.session_state['show']:
+        st.session_state['show'].append(show)
+        st.session_state['score'] = 0
+    
+    if value == "":
+        pass
+        #st.write("text field is empty")
+    
+    elif value in st.session_state['show'][-1]:
         st.balloons()
-        st.write(f"Correct! {value} is the right answer")
-    elif value == "":
-        st.write("text field is empty")
+        st.write(f"Correct! {st.session_state['show'][-1]} is the right answer")
+        st.session_state['score'] += 1
+
     else:
         st.write(f"wrong 😓 ({st.session_state['show'][-1]}) is the right answer")
-        
-        # add the next name to appear immedietly
-        st.session_state['show'].append(show.lower())    
 
+    # add the next name to appear immedietly
+    st.session_state['show'].append(show.lower())    
+
+    # adding the score counter
+    st.write('Score: ', st.session_state['score']*'🌟')
 
 def match_selection():
     '''Retrieve the show data (show, emojies)'''
@@ -63,7 +70,7 @@ def match_selection():
 
 # main code
 st.set_page_config("Guess The Show By Emoji", "✨", layout="wide")
-st.header("Guess Game Emoji", divider='blue')
+st.header("Guess The Show By Emojies", divider='blue')
 st.subheader("Choose a Category", divider='red')
 st.subheader("Based on the Emojies Bellow .. Guess The Name Of The Show")
 match_selection()
